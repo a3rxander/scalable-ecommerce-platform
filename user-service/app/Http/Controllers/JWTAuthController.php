9f10cd83ba3f4ca2d3305184c72da0ed381bf8cc
Alexander Agrazal <alexander.agrazal@tigo.com.pa>
2024-09-23 15:26:44 -0500

    Added role to User model for JWT authentication

--- user-service/app/Http/Controllers/JWTAuthController.php
+++ user-service/app/Http/Controllers/JWTAuthController.php
@@ -37,19 +37,25 @@ class JWTAuthController extends Controller
 
     // Inicio de sesión de usuario
     public function login(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
+{
+    $credentials = $request->only('email', 'password');
 
-        try {
-            if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'Credenciales inválidas'], 400);
-            }
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'No se pudo crear el token'], 500);
+    try {
+        if (! $token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
         }
 
+        // Obtener al usuario autenticado
+        $user = auth()->user();
+
+        // Agregar custom claims al token, incluyendo role
+        $token = JWTAuth::claims(['role' => $user->role])->fromUser($user);
+
         return response()->json(compact('token'));
+    } catch (JWTException $e) {
+        return response()->json(['error' => 'Could not create token'], 500);
     }
+}
 
     // Obtener el usuario autenticado
     public function getUser()
